d903b11f03971d4669242793755a78d77f2804ef
mino <bewriter310@gmail.com>
2020-09-13 06:20:07 +0900

    go

---
+++ codeup/9.12/1489.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void)
+{
+	int n,m,i,j,res=0,limit=0,col,row,arr[102][102],plus=-1;
+	scanf("%d %d", &n, &m);
+	row = n; 
+	col = m+1;
+	res = n * m;
+	while (res > 0)
+	{
+		for (i = 1; i <= m - limit; i++)
+		{
+			col += plus;
+			arr[row][col] = res--;
+		}			
+		for (i = 1; i < n - limit; i++)
+		{
+			row += plus;
+			arr[row][col] = res--;
+		}
+		plus *= -1;
+		limit++;
+	}
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= m; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
+	return 0;
+}

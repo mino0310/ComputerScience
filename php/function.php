aad6886ed2ac9c5b1163754135b8d9111000a473
minhokim <bewriter310@gmail.com>
2021-01-16 04:10:59 +0900

    PHP

---
+++ php/function.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>function</h1>
+        <?php
+            $str = "sleep 
+            eat 
+            smile";
+            echo nl2br($str);
+        ?>
+    </body>
+</html>
---
+++ php/string copy.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <h
+    <body>
+        <h1>String & String Operator</h1>
+        <?php
+            echo "Hello \"W\"orld";
+        ?>
+        <h2> concatenation operator</h2>
+        <?php
+            echo "Hello "."world";
+        ?>
+        <h2>String length</h2>
+        <?php
+            echo strlen("Hello, world");
+        ?>
+    </body>
+</html>

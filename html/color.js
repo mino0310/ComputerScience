0a45454a4574ae063a8ad22602cc2f043a4e414d
mino <bewriter310@gmail.com>
2021-01-15 15:36:43 +0900

    html/css/js

---
+++ html/color.js
@@ -0,0 +1,32 @@
+var Body = {
+    setColor: function(color) {
+        $('body').css('color', color);
+        // document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor(color) {
+        $('body').css('backgroundColor', color);
+        // document.querySelector('body').style.backgroundColor = color;
+    }
+
+}
+
+var Links = {
+    setColor: function(color) {
+        $('a').css('color', color);
+    }
+}
+
+function nightDayHandler(self) {
+    var target = document.querySelector('body');
+    if (self.value === 'night') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day';
+        Links.setColor('powderblue');
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value = 'night';
+        Links.setColor('blue');
+    }
+}

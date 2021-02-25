94af72c1d5562f285ac4a7e8f9ae9a87ed410b85
minhokim <bewriter310@gmail.com>
2021-02-25 12:32:42 +0900

    algo

---
+++ algorithm/15.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+
+bool isPrime(int x);
+int main(void)
+{
+    int n, cnt = 0;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++){
+        if (isPrime(i)) {
+            cnt++;
+        }
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
+
+bool isPrime(int x)
+{
+    int i = 2;
+    if (x == 1) return (false);
+    else if (x != 2 && x % 2 == 0) return (false);
+    while (i * i <= x) {
+        if (x % i == 0) return (false);
+        i++;
+    }
+    return (true);
+} 

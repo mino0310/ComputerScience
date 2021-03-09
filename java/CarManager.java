855c77c8134f90893f932d4b9624a887b829d063
mino0310 <bewriter310@gmail.com>
2021-03-10 08:40:43 +0900

    go

--- java/CarManager.java
+++ java/CarManager.java
@@ -15,3 +15,12 @@ public class Car {
 		return speed;
 	}
 }
+
+public class CarManager {
+	public static void main(String[] args) {
+		Car dogCar = new Car();
+		Car cowCar = new Car();
+		cowCar.speedUp();
+	}
+}
+

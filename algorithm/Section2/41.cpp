3f0d62288f3276dab4f345d0c2bcb98492a23b85
mino0310 <bewriter310@gmail.com>
2021-04-12 16:12:37 +0900

    algo study 4.12

--- algorithm/Section2/41.cpp
+++ algorithm/Section2/41.cpp
@@ -3,44 +3,34 @@
 #include <algorithm>
 #include <vector>
 
-
 int main(void)
 {
-    int n, num_size = 2, i, j, sum, right = 0, tmp_size, cnt = 0;
+    int i, j, k, n, input[1001] = {0, }, num_size = 2; 
+
     scanf("%d", &n);
-    std::vector<int> input(n+1);
 
     for (i = 1; i <= n; i++) {
         input[i] = i;
     }
 
     for (i = 1; i <= n; i++) {
-        sum = 0;
-        for (j = i ; j < i + num_size; j++) {
+        int sum = 0;
+        num_size += i;
+        j = 1;
+        while (num_size--) {
             sum += input[j];
-            if (sum == n) {
-                right = 1;
-                cnt++;
-            }
         }
-        printf("num_size = %d\n", num_size);
-        tmp_size = num_size;
-        if (right == 1) {
-            j = i;
-            while (j < num_size) {
-                printf("%d ", input[j]);
-                if (--tmp_size) {
-                    printf("+ ");
-                }
-                else {
-                    printf("= ");
-                }
-                j++;
-            }
-            printf("%d\n", n);
-        }
+        printf("sum = %d\n", sum);
+
     }
-    printf("%d", cnt);
     return 0;
 }
 
+
+/*
+쫄지 말고 다시 생각해보자
+
+
+
+
+*/
--- algorithm/Section2/42.cpp
+++ algorithm/Section2/42.cpp
@@ -4,23 +4,17 @@
 
 int main(void)
 {
-    int target, n, i, max_idx, min_idx, mid_idx, tmp;
-
+    int target, n, i, max_idx, min_idx = 0, mid_idx, tmp;
     scanf("%d %d", &n, &target);
-
-    std::vector<int> input(n + 1);
-
+    std::vector<int> input;
     for (i = 0; i < n; i++) {
         scanf("%d", &tmp);
         input.push_back(tmp);
     }
-        printf("11111\n");
     sort(input.begin(), input.end());
-    min_idx = 0;
     max_idx = n-1;
-    printf("zz");
     while (min_idx <= max_idx) {
-        min_idx = (min_idx + max_idx) / 2;
+        mid_idx = (min_idx + max_idx) / 2;
         if (input[mid_idx] == target) {
             printf("%d\n", mid_idx + 1);
             return 0;
@@ -28,18 +22,9 @@ int main(void)
         else if (input[mid_idx] > target) {
             max_idx = mid_idx -1;
         }
-        else min_idx = mid_idx + 1;
-        
-        // if (input[mid_idx] > target) {
-        //     max_idx = mid_idx - 1;
-        // }
-        // else if (input[mid_idx] < target) {
-        //     min_idx = mid_idx + 1;
-        // }
-        // else {
-        //     printf("%d", mid_idx + 1);
-        //     return 0;
-        // }
+        else {
+            min_idx = mid_idx + 1;
+        }
     }
     
     return 0;

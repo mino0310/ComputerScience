afcb6f257aac8cc69272e2a4033a99676bde4f03
mino0310 <bewriter310@gmail.com>
2021-04-29 12:52:06 +0900

    study

--- algorithm/Section2/44.cpp
+++ algorithm/Section2/44.cpp
@@ -3,40 +3,76 @@
 #include <algorithm>
 #include <vector>
 
+int make_cnt(int line[], int stall_num, int horse_num, int res);
 int main(void)
 {
-    int res, i, j, tmp, stall_num, horse_num, max_dst = 0, lt = 0, rt;
+    int mid, i, j, lt = 1, rt, res, cnt = 0, stall_num, horse_num, tmp;
 
     scanf("%d %d", &stall_num, &horse_num);
-    std::vector<int> stall_pos;
+    int *p = new int[stall_num + 1];
     
     for (i = 1; i <= stall_num; i++) {
-        scanf("%d", &tmp);
-        stall_pos.push_back(tmp);
+        scanf("%d", &p[i]);
     }
-    sort(stall_pos.begin(), stall_pos.end());
+    std::sort(p + 1, p + stall_num + 1);
 
-    rt = stall_num - 1;
+    rt = p[stall_num];
 
-    
-
-    // for (i = 0; i < stall_num; i++) {
-    //     printf("%d ", stall_pos[i]);
-    // }
-    // printf("\n");
-
-
-    // rt = stall_num;
-
+    while (lt <= rt) {
+        mid = (lt + rt) / 2;
+        if (make_cnt(p, stall_num, horse_num, mid) >= horse_num) {
+            res = mid;
+            lt = mid + 1;
+        }
+        else {
+            rt = mid - 1;
+        }
+    }
+    printf("%d", res);
+    delete[] p;
     return 0;
 }
 
+
+int make_cnt(int line[], int stall_num, int horse_num, int res) {
+    int cnt = 1, pos = 1, i;
+
+    i = 2; 
+    while (i < stall_num) {
+        if ((line[i] - line[pos]) >= res) {
+            pos = i;
+            cnt++;
+        }
+        i++;
+    }
+    return cnt;    
+}
+
 /*
-문제에서 요구하는 개념은 무엇인가 ?
+사고를 약간 반대로 해보면 되네 .
+세 개를 뽑아서 두 말의 거리를 측정한다는 식으로 문제에 접근하지 말고
 
-두 마구간 사이의 최소 거리를 한 값으로 결정해버리고 이분 탐색으로 옳은 값을 찾아나가면 되지 않나.
+가장 가까운 두 말의 거리의 최대값을 결정해두고 다른 말들을 이 거리 이상으로 배치하는 것이 가능한지를 알아보는 식으로 
+문제에 접근하는 것이 낫다.
 
-그 값은 순차적으로 정렬된 상태에서 찾아나가면 되는 것이니까.
+두 말의 거리가 최대가 되는 값을 찾는 문제.
+가장 가까운 말들의 거리를 정해놓고 그것보다는 다 멀리 있도록 말을 배치하는 것.
 
+그 값은 0부터 제일 큰 좌표값 사이에 있음.
 
+그리고 가운데 값이 답이 되는지 체크해보면 됨. 가운데 값은 1 + 9 를 2로 나눈 5.
+
+입력값으로 5 3 이라 했을 때 위에서 도출한 5( (1+9)/2) 는 무슨 의미냐?
+
+3마리의 말을 배치했을 때 가장 가까운 두 말 사이의 거리를 뜻하는 것. 
+다른 말들은 5 이상의 거리로 배치해야 하는 것.
+
+우선 수직선 1에 첫번째 말을 두고 5이상의 거리가 되는 지점을 찾는다. 
+그 후 그 지점에서 다시 5이상의 거리가 되는 지점을 찾아본다. 
+
+이렇게 했을 때 배치가 다 이루어지면 그보다 큰 값도 배치가 된다는 것이니 5보다 큰 범위에서 이분탐색으로 값을 찾아봐야 한다.
+배치가 이뤄지지 않으면 작은 값도 된다는 이야기니 그 값보다 아래의 범위를 다시 이분탐색으로 찾아봐야 한다.
+
+지금은 문제 해석 및 추상화 능력보다 코드화 능력이 더 떨어지므로 이를 집중적으로 훈련해야겠다.
 */
+

e069d470dd6bc87cf628d3ed89a30379e3f66a53
mino0310 <bewriter310@gmail.com>
2021-05-03 11:28:39 +0900

    java study

--- java/chapter_3_object/CarManager.java
+++ java/chapter_3_object/CarManager.java
@@ -1,26 +1,35 @@
-public class Car {
-	int speed;
-	int distance;
-	String color;
+// public class Car {
+// 	int speed;
+// 	int distance;
+// 	String color;
 
-	public Car() {
-	}
-	public void speedUp() {
-		speed += 5;
-	}
-	public void breakDown() {
-		speed -= 10;
-	}
-	public int getCurrentSpeed() {
-		return speed;
-	}
-}
+// 	public Car() {
+// 	}
+// 	public void speedUp() {
+// 		speed += 5;
+// 	}
+// 	public void breakDown() {
+// 		speed -= 10;
+// 	}
+// 	public int getCurrentSpeed() {
+// 		return speed;
+// 	}
+// }
 
+// public class CarManager {
+// 	public static void main(String[] args) {
+// 		Car dogCar = new Car();
+// 		Car cowCar = new Car();
+// 		cowCar.speedUp();
+// 	}
+// }
 public class CarManager {
 	public static void main(String[] args) {
 		Car dogCar = new Car();
-		Car cowCar = new Car();
-		cowCar.speedUp();
+		dogCar.speedUp();
+		dogCar.speedUp();
+		System.out.println(dogCar.getCurrentSpeed());
+		dogCar.breakDown();
+		System.out.println(dogCar.getCurrentSpeed());
 	}
 }
-

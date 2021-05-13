101b3d54c14123343e4d406dcaaa5f0cf2525751
mino0310 <bewriter310@gmail.com>
2021-05-13 22:28:59 +0900

    algo study

--- algorithm/Section2/52.cpp
+++ algorithm/Section2/52.cpp
@@ -3,40 +3,69 @@
 #include <algorithm>
 #include <vector>
 
+int my_min(int a, int b, int c);
+
 using namespace std;
 
-int arr[1500] = {1, };
+int arr[1501];
 
 int main(void)
 {
-    int n, i, p1, p2, p3, min = 0;
-    p1 = p2 = p3 = 0;
-
+    int n, i, p2, p3, p5, min = 0, min_idx = 0;
+    p2 = p3 = p5 = 1;
+    arr[1] = 1;
     cin >> n;
 
-    for (i = 1; i < n; i++) {
-        printf("arr[p1] = %d, arr[p2] = %d, arr[p3] = %d\n", arr[p1], arr[p2], arr[p3]);
-        arr[p1] *= 2;
-        arr[p2] *= 3;
-        arr[p3] *= 5;
-        min = (arr[p1] > arr[p2]) ? ((arr[p2] < arr[p3]) ? arr[p3] : arr[p2]) : (arr[p1] < arr[p3]) ? arr[p1] : ((arr[p3] < arr[p2]) ? arr[p3] : arr[p2]);
-        //min = (arr[p1] > arr[p2]) ? (arr[p2] < arr[p3]) ? arr[p2] : ((arr[p1] > arr[p3]) ? arr[p3] : arr[p1]);
+    for (i = 2; i <= n; i++) {
+        
+        min = my_min(arr[p2]*2, arr[p3]*3, arr[p5]*5);
+        if (arr[p2]*2 == min) p2++;
+        if (arr[p3]*3 == min) p3++;
+        if (arr[p5]*5 == min) p5++;
         arr[i] = min;
-        if (arr[p1] == min && arr[p2] == min) p1++, p2++;
-        else if (arr[p2] == min && arr[p3] == min) p2++, p3++;
-        else if (arr[p3] == min && arr[p1] == min) p3++, p1++;
-        else if (arr[p1] == min) p1++;
-        else if (arr[p2] == min) p2++;
-        else p3++;
-
     }
-
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    cout << arr[n] << endl;
     return 0;
 }
 
+int my_min(int a, int b, int c) {
+    if(a > b) {
+        if (b > c) {
+            return c;
+        }
+        else return b;
+    }
+    else {
+        if (a > c) {
+            return c;
+        }
+        else
+            return a;
+    }
+}
+/*
+1 2 3 4 5 6 8 9 10 12 15 
+1 2 3 2*2 5 2*3 2*4 3*3 2*5 3*2*2 3*5 2*2*2*2
+
+2 3 5 뢰 되어있는 배열이라는 건
+2 3 5를 곱햏서 만들 수 있다는 말
+어떤 규칙성을 가지지?
+1 은 제외
+2 - 1*2     
+3 - 1*3     
+4 - 2*2     
+5 - 1*5     
+6 - 2*3     
+8 - 2*2*2   
+9 - 3*3     
+10 - 2 * 5
+12 - 2*2*3
+15 - 3*5
+
+
+
+
+*/
 
 /* 
 소인수 분해의 결과는 무엇을 의미하지? 소수의 곱으로 나타낸 것이지. 

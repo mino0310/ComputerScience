11f536e891ae957f04b5c897324a743cbab83413
mino <bewriter310@gmail.com>
2021-05-31 22:43:59 +0900

    algo study

---
+++ algorithm/Section3/57.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+
+using namespace std;
+stack<int> s;
+
+void digit(int n) 
+{
+    if (n == 0) {
+        return ;
+    }
+    s.push(n%2);
+    digit(n / 2);
+    
+}
+
+int main(void)
+{
+    int n;
+    cin>>n;
+    digit(n);
+    while(!s.empty())
+    {
+        cout << s.top();
+        s.pop();
+    }
+
+    return 0;
+}

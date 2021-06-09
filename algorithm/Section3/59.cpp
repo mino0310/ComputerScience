b2a962e194cd2b23cf037da94e24a2e90a3f9b54
mino0310 <bewriter310@gmail.com>
2021-06-10 07:55:32 +0900

    study

--- algorithm/Section3/59.cpp
+++ algorithm/Section3/59.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
-#include <stack>
 
 using namespace std;
-int n;
+int n, check[11];
 
-void D(int v)
+void DFS(int v)
 {
-    if (v > n) return ;
-    cout << v << " ";
-    D(2*v);
-    D(2*v + 1);
+    if (v == n + 1) {
+        for (int i = 1; i <= n; i++) {
+            if (check[i]) {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+        return ;
+    }
+    else {
+        check[v] = 1;
+        DFS(v+1);
+        
+        check[v] = 0;
+        DFS(v+1);
+    }
 }
-
 
 int main(void)
 {
     cin >> n;
-    for (int i = 1; i <= 3; i++)
-    {
-        D(i);
-        cout << endl;
-    }
     
+    DFS(1);
+
     return 0;
 }

6b4d1b9da06077353c104005bcda38e7014f63a4
mino0310 <bewriter310@gmail.com>
2021-06-18 15:50:31 +0900

    merge_sort_ver_pro

--- algorithm/Section3/merge_sort.cpp
+++ algorithm/Section3/merge_sort.cpp
@@ -4,35 +4,64 @@ int sorted[MAX_SIZE];
 
 using namespace std;
 
-void merge(int list[], int left, int mid, int right)
+// void merge(int list[], int left, int mid, int right)
+// {
+//     int i, j, k, l;
+//     i = left;
+//     j = mid + 1;
+//     k = left;
+
+//     while (i <= mid && j <= right) {
+//         if (list[i] <= list[j])
+//             sorted[k++] = list[i++];
+//         else
+//             sorted[k++] = list[j++];
+//     }
+
+//     if (i > mid) {
+//         for (l = j; l <= right; l++) {
+//             sorted[k++] = list[l];
+//         }
+//     } else {
+//         for (l = i; l <= mid; l++) {
+//             sorted[k++] = list[l];
+//         }
+//     }
+
+//     for (l = left; l <= right; l++) {
+//         list[l] = sorted[l];
+//     }
+// }
+
+void merge(int list[], int start, int mid, int end)
 {
-    int i, j, k, l;
-    i = left;
+    int i, j, k;
+    i = start;
     j = mid + 1;
-    k = left;
+    k = start;
 
-    while (i <= mid && j <= right) {
-        if (list[i] <= list[j])
-            sorted[k++] = list[i++];
-        else
+    // 대소 비교하면서 합치기
+    while (i <= mid && j <= end) {
+        if (list[i] > list[j]) {
             sorted[k++] = list[j++];
+        }
+        else {
+            sorted[k++] = list[i++];
+        }
     }
-
     if (i > mid) {
-        for (l = j; l <= right; l++) {
-            sorted[k++] = list[l];
+        while (j <= end) {
+            sorted[k++] = list[j++];
         }
     } else {
-        for (l = i; l <= mid; l++) {
-            sorted[k++] = list[l];
+        while (i <= mid) {
+            sorted[k++] = list[i++];
         }
     }
-
-    for (l = left; l <= right; l++) {
-        list[l] = sorted[l];
+    for (i = start; i <= end; i++) {
+        list[i] = sorted[i];
     }
 }
-
 void merge_sort(int list[], int left, int right)
 {
     int mid;

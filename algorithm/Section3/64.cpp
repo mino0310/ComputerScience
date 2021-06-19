a1d12a2d59ba599f348093999cc4a425baddb9ae
mino <bewriter310@gmail.com>
2021-06-19 14:24:30 +0900

    algo study

--- algorithm/Section3/64.cpp
+++ algorithm/Section3/64.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
 
-int adj_matrix[21][21], cnt;
+int adj_matrix[21][21], cnt, n, visited[100];
 using namespace std;
 
-void dfs(int n)
+void dfs(int l, int vertex)
 {
-    
+    if (vertex == 0 || l == n + 1) return ;
+    if (vertex == n) {
+        cnt++;        
+        return ;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (adj_matrix[vertex][i] == 1 && !visited[i]) {
+            visited[i] = 1;
+            dfs(l + 1, i);
+            visited[i] = 0;
+        } else {
+            dfs(l + 1, 0);
+        }   
+    }
 }
 
 
 int main(void)
 {
-    int n, m, i, row, col;
+    int m, i, row, col;
     cin >> n >> m;
 
     for (i = 1; i <= m; i++) {
         cin >> row >> col;
         adj_matrix[row][col] = 1;
     }
-
-    for (i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++) {
-            cout << adj_matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    dfs(1);
+    visited[1] = 1;
+    dfs(1, 1);
     cout << cnt << endl;
     return 0;
 }

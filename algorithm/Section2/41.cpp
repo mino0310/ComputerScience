c0b67ca8e8649184e1b223152acd116fb3ebec73
mino0310 <bewriter310@gmail.com>
2021-06-24 11:39:23 +0900

    algo study

--- algorithm/Section2/41.cpp
+++ algorithm/Section2/41.cpp
@@ -1,36 +1,14 @@
-#include <stdio.h>
 #include <iostream>
-#include <algorithm>
-#include <vector>
+
+int cnt;
 
 int main(void)
 {
-    int i, j, k, n, input[1001] = {0, }, num_size = 2; 
+    int n;
 
-    scanf("%d", &n);
+    cin >> n;
+    
 
-    for (i = 1; i <= n; i++) {
-        input[i] = i;
-    }
-
-    for (i = 1; i <= n; i++) {
-        int sum = 0;
-        num_size += i;
-        j = 1;
-        while (num_size--) {
-            sum += input[j];
-        }
-        printf("sum = %d\n", sum);
-
-    }
+    cout << cnt << endl;
     return 0;
 }
-
-
-/*
-쫄지 말고 다시 생각해보자
-
-
-
-
-*/

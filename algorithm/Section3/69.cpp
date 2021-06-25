95c55636dd16d95960f0314e4616f7482cbb481d
mino0310 <bewriter310@gmail.com>
2021-06-25 13:33:33 +0900

    bfs_study

---
+++ algorithm/Section3/69.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+vector<int> adj_list[8];
+int check[8];
+
+
+void bfs(int node)
+{
+    queue<int> q;
+    int i, x, y;
+    q.push(node);
+    check[node] = 1;
+
+    while (!q.empty()) {
+        x = q.front();
+        q.pop();
+
+        cout << x << " ";
+        for (i = 0; i < adj_list[x].size(); i++) {
+            if (check[adj_list[x][i]] == 0)
+            {
+                q.push(adj_list[x][i]);
+                check[adj_list[x][i]] = 1;
+            }
+        }
+    }
+
+
+}
+
+int main(void)
+{
+    int i, node, linked_node;
+    for (i = 0; i < 6; i++) {
+        cin >> node >> linked_node;
+        adj_list[node].push_back(linked_node);
+    }
+    // check[1] = 1;
+    bfs(1);
+    return 0;
+}
---
+++ algorithm/Section3/bfs_adj_matrix.cpp
@@ -0,0 +1,13 @@
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int adj_matrix[10][10], check[10];
+
+
+
+int main(void)
+{
+    return 0;
+}

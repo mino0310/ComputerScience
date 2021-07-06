596cd7b422dafc9ed857a5ddb37d356de21194a9
mino <bewriter310@gmail.com>
2021-07-06 17:36:49 +0900

    algo_solved

--- algorithm/Section3/72.cpp
+++ algorithm/Section3/72.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <queue>
 
+using namespace std;
+
 int main(void)
 {
-    int n, k, i;
+    queue<int> q;
+    int n, k, i, x, cnt = 0;
  
     cin >> n >> k;
+
+    for (i = 1; i <= n; i++)
+        q.push(i);
+
+    while (!q.empty())
+    {
+        x = q.front();
+        q.pop();
+        cnt++;
+        if (cnt == k) 
+        {
+            cnt = 0;
+            continue;
+        }
+        else
+            q.push(x);
+    }
+    cout << x << endl;
 
     return 0;
 }

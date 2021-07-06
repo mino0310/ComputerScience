db4cae995d56f1b54f8707ba7a9a40aed3299228
mino0310 <bewriter310@gmail.com>
2021-07-06 16:56:13 +0900

    algo_study

--- algorithm/Section3/71.cpp
+++ algorithm/Section3/71.cpp
@@ -4,36 +4,37 @@
 
 using namespace std;
 
-int n, m, cnt;
-
-
-void bfs(int start, int level)
-{
-    queue<int> q;
-    q.push(start);
-
-    while (!q.empty())
-    {
-        int x = q.front();
-        if (x == m) 
-        {
-            cout << "zzzz\n";
-            break ;
-        }
-        q.pop();
-        q.push(x + 1);
-        q.push(x - 1);
-        q.push(x + 5);
-    }
-}
+int n, m, cnt, check[10001];
 
 int main(void)
 {
-    cin >> n >> m;
-    
-    bfs(n, 0);
-    
-    cout << cnt << endl;
+    int d[3] = {1, -1, 5}, pos;
+    queue<int> q;
 
+    cin >> n >> m;
+    check[n] = 1;
+    q.push(n);
+    while (!q.empty())
+    {
+        int x = q.front();
+
+        q.pop();
+        for (int i = 0; i < 3; i++)
+        {
+            pos = x + d[i];
+            
+            if (pos > 100000 || pos < 1)
+                continue;
+            if (check[pos] == 0)
+            {
+                check[pos] = check[x] + 1;
+                q.push(pos);    
+            }
+            else if (pos == m) {
+                cout << check[x] << endl;
+                exit(0);
+            }
+        }
+    }
     return 0;
 }

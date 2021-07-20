2409b11ede92635e8602285992eb5623a84bec14
mino0310 <bewriter310@gmail.com>
2021-07-20 22:23:07 +0900

    algo study

---
+++ algorithm/Section4/78.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(void)
+{
+    int v, e, city_a, city_b, cost;
+
+    cin >> v >> e;
+    for (int i = 0; i < e; i++)
+    {
+        cin >> city_a >> city_b >> cost;
+    }
+    return 0;
+}

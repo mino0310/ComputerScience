2bfd5bea45fac67a5334f58081d62d2b9945dd97
mino0310 <bewriter310@gmail.com>
2021-07-25 04:06:00 +0900

    algo study

--- algorithm/Section4/79.cpp
+++ algorithm/Section4/79.cpp
@@ -1,14 +1,53 @@
 #include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
 
 using namespace std;
+int ch[30];
+
+struct Edge {
+    int e;
+    int val;
+    Edge(int a, int b){
+        e = a;
+        val = b;
+    }
+    bool operator<(const Edge &b)const {
+        return val > b.val;
+    }
+};
+
 
 int main(void)
 {
-    int n, m;
-
+    priority_queue<Edge> Q;
+    vector<pair<int, int> > map[30];
+    int i, n, m, a, b, c, res = 0;
     cin >> n >> m;
+    for (i = 1; i <= m; i++)
+    {
+        cin >> a >> b >> c;
+        map[a].push_back(make_pair(b, c));
+        map[b].push_back(make_pair(a, c));
+    }
+    Q.push(Edge(1, 0));
+    while(!Q.empty())
+    {
+        Edge tmp = Q.top();
+        Q.pop();
+        int v = tmp.e;
+        int cost = tmp.val;
+        if (ch[v] == 0) {
+            res += cost;
+            ch[v] = 1;
+            for (i = 0; i < map[v].size(); i++)
+            {
+                Q.push(Edge(map[v][i].first, map[v][i].second));
+            }
+        }
+    }
+    cout << res << endl;
 
-    cout << n << m;
     return 0;
-
 }

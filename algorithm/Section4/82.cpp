3ece3273e6b05b6826508b2fa4400c26742a4800
mino0310 <bewriter310@gmail.com>
2021-08-02 19:15:49 +0900

    DFS permutation

---
+++ algorithm/Section4/82.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int num[15], n, r, cnt, check[15];
+
+void DFS(int idx, int cnt)
+{
+    if (end_condition) {
+        return ;
+    }
+    else {
+        
+    }
+}
+
+int main(void)
+{
+    int i;
+    cin >> n >> r;
+
+    for (i = 1; i <= n; i++){
+        cin >> num[i];
+    }
+    // check[1] = 1;
+    DFS(1, 1);
+    
+
+
+    return 0;
+}
